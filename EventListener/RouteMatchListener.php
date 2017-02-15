f16f53bed506bbea07308142a6a5ec9db9f0bfb8
szymon <szymon.kunowski@gmail.com>
2017-02-15 13:31:25 +0100

    add role check event

--- EventListener/RouteMatchListener.php
+++ EventListener/RouteMatchListener.php
@@ -3,19 +3,42 @@
 
 namespace Opstalent\SecurityBundle\EventListener;
 
+use Symfony\Bundle\FrameworkBundle\Routing\Router;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpKernel\Event\FilterControllerEvent;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorage;
 
 
 class RouteMatchListener
 {
+    protected $router;
+    protected $tokenStorage;
 
-    public function onKernelRequest(GetResponseEvent $event)
+    public function __construct(Router $router, TokenStorage $tokenStorage) // this is @service_container
+    {
+        $this->router = $router;
+        $this->tokenStorage = $tokenStorage;
+    }
+
+    public function onKernelRequest(FilterControllerEvent $event)
     {
         $request = $event->getRequest();
-        $route = $this->get('router')->getRouteCollection()->get($request->attributes->get('_route'));
-        dump($route->getOption('security'));
+        $route = $this->router->getRouteCollection()->get($request->attributes->get('_route'));
+        if($route && is_array($options = $route->getOption('security'))) {
+            if(!$this->canAccess($options)) {
+                throw new \Exception("Forbidden",403)  ;
+            }
+        }
+    }
+
+    protected function canAccess(array $options):bool
+    {
+        if(array_key_exists('roles', $options) && !empty($options['roles']) > 0 && $this->tokenStorage->getToken() && $user = $this->tokenStorage->getToken()->getUser()) {
+            return !empty(array_intersect($options['roles'],$user->getRoles()));
+        }
 
     }
 
